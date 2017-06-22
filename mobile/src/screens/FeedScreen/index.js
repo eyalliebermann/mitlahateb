e69f67995092d63a62339dac3c25494125e0d11f
eyalliebermann <eyal.liebermann@gmail.com>
2017-06-22 22:40:38 +0300

    feed

--- mobile/src/screens/FeedScreen/index.js
+++ mobile/src/screens/FeedScreen/index.js
@@ -1,10 +1,115 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {View, Text, TextInput, TouchableHighlight, StyleSheet, Button, Modal} from 'react-native';
+import {NavigationActions} from 'react-navigation';
+import TagInput from 'react-native-taginput';
 
-export default function FeedScreen() {
-    return <Text>This is the feed!</Text>
+const submitNavigationAction = NavigationActions.reset({
+    index: 0,
+    actions: [NavigationActions.navigate({routeName: 'Feed'})]
+});
+
+export default class FeedScreen extends React.Component {
+    static defaultProps = {
+        skillOptions: ['Graphic Design', 'Software Development', 'IT', 'Physical Strength']
+    };
+
+     state = {
+    modalVisible: false,
+  }
+
+    constructor(props) {
+        super(props);
+
+        // const {navigation: {state: {params: {profile}}}} = props;
+        // this.state = {
+        //     skills: [],
+        //     ...profile
+        // };
+    }
+
+    linkState = stateName => ({
+        // onChange: e => this.setState({[stateName]: e.nativeEvent.text}),
+        // value: this.state[stateName]
+    });
+
+    handleSubmit = () => {
+        // perform some request to server side with fetch, and then navigate to feed
+      //  this.props.navigation.dispatch(submitNavigationAction);
+    };
+
+    onPressTitle = () =>{
+        this.setModalVisible(true);
+    }
+
+
+setModalVisible(visible) {
+    this.setState({modalVisible: visible});
+  }
+    render() {
+        const {skillOptions} = this.props;
+
+        let taskItems = [{
+            'id':1,
+            'title': 'Hosting a guy in need!',
+            'description': 'Host an f2m guy in need tonight! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent volutpat aliquet ante, ac suscipit odio consectetur in. Donec elementum nibh id congue venenatis. Etiam mattis et ex non blandit. Donec sed vestibulum neque. Ut egestas odio et sapien volutpat, ac laoreet odio placerat. Maecenas id metus volutpat, scelerisque magna eget, fermentum justo. Fusce quis mauris et elit luctus accumsan. Nunc sed nibh et nisi euismod pharetra. Morbi pretium hendrerit posuere. Aenean congue elit at convallis faucibus.',
+            'date': '2017-06-21' 
+        },{
+            'id':3,
+            'title': 'Last Minute Call for securing Pride!',
+            'description': 'Host an f2m guy in need tonight! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent volutpat aliquet ante, ac suscipit odio consectetur in. Donec elementum nibh id congue venenatis. Etiam mattis et ex non blandit. Donec sed vestibulum neque. Ut egestas odio et sapien volutpat, ac laoreet odio placerat. Maecenas id metus volutpat, scelerisque magna eget, fermentum justo. Fusce quis mauris et elit luctus accumsan. Nunc sed nibh et nisi euismod pharetra. Morbi pretium hendrerit posuere. Aenean congue elit at convallis faucibus.',
+            'date': '2017-06-22' 
+        },{
+            'id':4,
+            'title': 'Missing story teller on Hoshen tommorow',
+            'description': 'Host an f2m guy in need tonight! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent volutpat aliquet ante, ac suscipit odio consectetur in. Donec elementum nibh id congue venenatis. Etiam mattis et ex non blandit. Donec sed vestibulum neque. Ut egestas odio et sapien volutpat, ac laoreet odio placerat. Maecenas id metus volutpat, scelerisque magna eget, fermentum justo. Fusce quis mauris et elit luctus accumsan. Nunc sed nibh et nisi euismod pharetra. Morbi pretium hendrerit posuere. Aenean congue elit at convallis faucibus.',
+            'when': '2017-06-23' 
+        }];
+
+            let tasks = taskItems.map( (task) =>{ return (
+              <Text key={task.id} style={styles.baseText}>
+              <Text style={styles.titleText} onPress={this.onPressTitle}>
+                    {task.title}{'\n'}
+                </Text>
+                <Text numberOfLines={3}>
+                    {task.description}{'\n'}
+                </Text>
+                 <Text>
+                    {task.date}
+                </Text>
+                {'\n'}
+                {'\n'}
+                </Text>
+             );
+            });
+
+        return (
+            <View style={styles.view} >
+             {tasks}
+            </View>
+            
+        );
+    }
 }
+
+const styles = StyleSheet.create({
+  baseText: {
+    fontFamily: 'Cochin',
+  },
+  titleText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+    view: {
+        display: 'block',
+        top: 22,
+        bottom:22,
+        marginTop: 22,
+        marginHorizontal: '10%',
+        justifyContent: 'center'
+    }
+});
+
 
 FeedScreen.navigationOptions = {
     title: 'Feed'
-}
+};
